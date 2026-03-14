f6e4f065d654b020aa6e6febcb2a4f28433a6078
Iceheop <20231133@itla.edu.do>
2026-03-14 13:44:00 -0400

    Update site URL in Astro config and add deployment workflow for cPanel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://zoodomi.netlify.app',
+    site: 'https://www.zoodom.lat',
 });
