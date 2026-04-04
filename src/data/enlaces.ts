11e4e5d5f1e515d9fe334e84c9dc0ddace2349d7
Iceheop <20231133@itla.edu.do>
2026-04-04 14:04:34 -0400

    feat: Add image dimensions to enlaces and update logo alt attributes for better accessibility

--- src/data/enlaces.ts
+++ src/data/enlaces.ts
@@ -10,6 +10,8 @@ export type Enlace = {
     title: string;
     image: string;
     imageAlt: string;
+    imageWidth: number;
+    imageHeight: number;
     url: string;
 };
 
@@ -18,42 +20,56 @@ export const enlaces: Enlace[] = [
         title: "Linea de Atencion Ciudadana",
         image: lineaAtencionCiudadanaIcon,
         imageAlt: "Icono de linea de atencion ciudadana",
-        url: "#"
+        imageWidth: 238,
+        imageHeight: 191,
+        url: "https://311.gob.do/"
     },
     {
         title: "Gobierno Abierto",
         image: gobiernoAbiertoIcon,
         imageAlt: "Icono de gobierno abierto",
-        url: "#"
+        imageWidth: 191,
+        imageHeight: 191,
+        url: "https://gobiernoabierto.gob.do/"
     },
     {
         title: "Emergencias 911",
         image: emergencias911Icon,
         imageAlt: "Icono de emergencias 911",
-        url: "#"
+        imageWidth: 223,
+        imageHeight: 191,
+        url: "https://911.gob.do/"
     },
     {
         title: "Logo Nuevo",
         image: logoNuevoIcon,
-        imageAlt: "Logo institucional nuevo",
-        url: "#"
+        imageAlt: "Oficina Gubernamental de Tecnologías de la Información y Comunicación",
+        imageWidth: 497,
+        imageHeight: 191,
+        url: "https://ogtic.gob.do/"
     },
     {
         title: "Logo de Mapa",
         image: logoMapaIcon,
-        imageAlt: "Logo de mapa",
-        url: "#"
+        imageAlt: "gobierno de la república dominicana",
+        imageWidth: 310,
+        imageHeight: 191,
+        url: "https://www.gob.do/"
     },
     {
         title: "Logo SAIP",
         image: logoSaipIcon,
-        imageAlt: "Logo SAIP",
-        url: "#"
+        imageAlt: "Portal Único de Solicitud de Acceso a la Información Pública",
+        imageWidth: 740,
+        imageHeight: 191,
+        url: "https://saip.gob.do/"
     },
     {
         title: "Logo Web Azul",
         image: logoAzulIcon,
-        imageAlt: "Logo web azul",
-        url: "#"
+        imageAlt: "Ministerio publico",
+        imageWidth: 408,
+        imageHeight: 191,
+        url: "https://pgr.gob.do/"
     }
 ];
